16a78406383c88c8f30a8d4068308751de2d569f
Medeiros <hsymetry@gmail.com>
2022-07-23 23:51:13 -0300

    Added font

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
+      theme: ThemeData(
+        fontFamily: 'SourceSansPro-Regular',
         // primaryColor: Colors.grey,
         scaffoldBackgroundColor: const Color(0xFFFFFFFF),
         appBarTheme: const AppBarTheme(
